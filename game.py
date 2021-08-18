0a47ac3754bdc2d099d8dd8c544f73fdb45344d3
DJBrause <pawnie86@gmail.com>
2021-08-18 13:20:42 +0200

    Player class moved to different file

--- game.py
+++ game.py
@@ -1,134 +1,21 @@
 import pygame
 import math
 import random
+from ship_class import Player
 
 pygame.init()
 screen = pygame.display.set_mode((1600, 1000))
 list_of_ships = []
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        self.moving_to_wp = False
-        self.active_waypoint = None
-        self.coord_x = x
-        self.coord_y = y
-        self.active = False
-        self.ship_collision = False
-        self.colliding_element = None
-        super().__init__()
-        self.og_image = pygame.image.load(r'graphics\statek_clear.png').convert_alpha()
-        self.image = pygame.transform.scale(self.og_image, (51, 51))
-        self.rect = self.image.get_rect(center=(self.coord_x, self.coord_y))
-        self.waypoints = []
-
-    def test(self):
-        print(f"len list = {len(self.waypoints)}")
-
-    def transformed_image(self):
-        self.image = pygame.transform.scale(self.og_image, (51, 51))
-        return self.image
-    '''Sets the ship into active state where it can react to orders.'''
-
-    def activate_ship(self):
-        self.active = True
-        if self.moving_to_wp is False:
-            self.active_waypoint = (self.coord_x, self.coord_y)
-
-    def deactivate_ship(self):
-        self.active = False
-
-    def new_waypoint(self, wp):
-        self.waypoints = []
-        self.active_waypoint = wp
-        self.rotate()
-
-    def new_waypoint_keep_list(self, wp):
-        self.active_waypoint = wp
-        self.rotate()
-
-    def rotate(self):
-        if self.active_waypoint is not None:
-            waypoint_x, waypoint_y = self.active_waypoint[0], self.active_waypoint[1]
-            rel_x, rel_y = waypoint_x - (self.rect.x + 51), waypoint_y - self.rect.y
-            angle = (180 / math.pi) * -math.atan2(rel_y, rel_x) + 270
-
-            self.image = pygame.transform.rotate(self.transformed_image(), int(angle))
-            self.rect = self.image.get_rect(center=(self.coord_x, self.coord_y))
-
-    def go_to_waypoint(self):
-        if self.active_waypoint is not None:
-            self.dist = math.sqrt((self.coord_x - self.active_waypoint[0]) ** 2 + (self.coord_y - self.active_waypoint[1]) ** 2)
-            limit = 1200
-            reduction = 100
-            wp_vicinity = 30
-
-            '''dopracować'''
-            x_move = (self.active_waypoint[0] - self.coord_x)
-            y_move = (self.active_waypoint[1] - self.coord_y)
-            distance = abs(x_move + y_move)
-            if distance > limit:
-                reduction += (abs(distance)-limit)
-            ''''''
-
-            if self.dist > wp_vicinity:
-                self.moving_to_wp = True
-                self.coord_x += x_move / reduction
-                self.coord_y += y_move / reduction
-
-            elif self.dist <= wp_vicinity:
-                if len(self.waypoints) == 1:
-                    self.final_leg()
-
-                else:
-                    self.go_to_next_wp()
-
-        self.rect = self.image.get_rect(center=(self.coord_x, self.coord_y))
-
-    def final_leg(self):
-        self.waypoints = []
-        print(len(self.waypoints))
-        self.bump()
-        if self.ship_collision is False:
-            self.moving_to_wp = False
-            self.active_waypoint = None
-        elif self.ship_collision is True:
-            self.change_wp_upon_collision()
-
-    def bump(self):
-        for ship in list_of_ships:
-            for item in ship.sprites():
-                if item != self.rect and item.rect.colliderect(self.rect):
-                    self.ship_collision = True
-                    self.colliding_element = item
-
-    def go_to_next_wp(self):
-        if len(self.waypoints) > 1:
-            self.waypoints.remove(self.active_waypoint)
-            print(len(self.waypoints))
-            self.active_waypoint = self.waypoints[0]
-            self.rotate()
-
-    def change_wp_upon_collision(self):
-
-        new_wp_x = self.coord_x + random.randrange(-150, 150, 50)
-        new_wp_y = self.coord_y + random.randrange(-150, 150, 50)
-
-        self.active_waypoint = (new_wp_x, new_wp_y)
-        self.waypoints.append(self.active_waypoint)
-        self.rotate()
-        self.ship_collision = False
-
-    def update(self):
-        self.go_to_waypoint()
-
+scroll = [0, 0]
 
 class Station(pygame.sprite.Sprite):
     def __init__(self):
+        self.x_coord = 300
+        self.y_coord = 200
         super().__init__()
         self.image = pygame.image.load(r'graphics\spacestation\WB_baseu3_d0.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (150, 150))
-        self.rect = self.image.get_rect(midbottom=(300, 200))
+        self.rect = self.image.get_rect(midbottom=(self.x_coord, self.y_coord))
 
     def move(self):
         pass
@@ -136,7 +23,7 @@ class Station(pygame.sprite.Sprite):
     def update(self):
         pass
 
-
+# Two super junky functions. To be replaced with dynamic ship generation.
 def ship_generator():
     for i in range(10):
         x = random.randint(0, 1600)
@@ -145,8 +32,13 @@ def ship_generator():
         player.add(Player(x, y))
         list_of_ships.append(player)
 
+def add_ship_list_to_ships():
+    for sprite in list_of_ships:
+        for ship in sprite.sprites():
+            ship.list_of_ships = list_of_ships
 
 ship_generator()
+add_ship_list_to_ships()
 
 station = pygame.sprite.GroupSingle(sprite=Station())
 station.add(Station())
@@ -165,7 +57,7 @@ def main():
 
     while True:
         screen.blit(background, (0, 0))
-
+        screen_scrolling()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -195,7 +87,7 @@ def main():
             height = -(selection_box_start[1] - selection_box_end[1])
             selection = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(selection_box_start[0], selection_box_start[1]
                                                                           , width, height), 2)
-
+        add_scroll_to_ship_pos()
         draw_ships()
         station.draw(screen)
 
@@ -209,6 +101,19 @@ def draw_ships():
         ship.update()
 
 
+def add_scroll_to_ship_pos():
+    for rectangle in station.sprites():
+        rectangle.rect.x -= scroll[0]
+        rectangle.rect.y -= scroll[1]
+    for sprite in list_of_ships:
+        for rectangle_ship in sprite.sprites():
+            rectangle_ship.coord_x -= scroll[0]
+            rectangle_ship.coord_y -= scroll[1]
+            rectangle_ship.wp_modifier_x -= scroll[0]
+            rectangle_ship.wp_modifier_y -= scroll[1]
+            if rectangle_ship.active_waypoint is not None:
+                rectangle_ship.active_waypoint = ((rectangle_ship.active_waypoint[0] - scroll[0]), (rectangle_ship.active_waypoint[1] - scroll[1]))
+
 def forward_wp_to_active_ship(wp):
     for ship in list_of_ships:
         for item in ship.sprites():
@@ -221,10 +126,14 @@ def forward_wp_to_active_ship_list(wp):
     for ship in list_of_ships:
         for item in ship.sprites():
             if item.active is True:
+                # adding wp modifier to take into account scrolling
+                print(item.wp_modifier_x)
+                wp = (wp[0] - item.wp_modifier_x, wp[1] - item.wp_modifier_y)
                 item.waypoints.append(wp)
                 print(len(item.waypoints))
                 if len(item.waypoints) == 1:
                     item.new_waypoint_keep_list(wp)
+
 
 def collision_check(rect):
     if type(rect) == tuple:
@@ -241,6 +150,25 @@ def collision_check(rect):
                     item.activate_ship()
                 else:
                     item.deactivate_ship()
+
+
+def screen_scrolling():
+    global scroll
+    mouse_pos = pygame.mouse.get_pos()
+    if mouse_pos[0] < 20:
+        scroll[0] -= 1
+        print("scrolling left")
+    elif mouse_pos[0] > 1579:
+        scroll[0] += 1
+        print("scrolling right")
+    elif mouse_pos[1] < 20:
+        scroll[1] -= 1
+        print("scrolling down")
+    elif mouse_pos[1] > 979:
+        scroll[1] += 1
+        print("scrolling up")
+    else:
+        scroll = [0, 0]
 
 
 if __name__ == "__main__":
